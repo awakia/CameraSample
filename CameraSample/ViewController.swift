99df5c284657f214041ace3c25a395547d3edb78
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2018-09-12 16:57:50 +0900

    Enable to show camera

--- CameraSample/ViewController.swift
+++ CameraSample/ViewController.swift
@@ -9,49 +9,32 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     var device: AVCaptureDevice!
     var session: AVCaptureSession!
-    var output: AVCaptureStillImageOutput!
+    var output: AVCaptureVideoDataOutput!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // セッションを生成
+
         session = AVCaptureSession()
-        // バックカメラを選択
-        for d in AVCaptureDevice.devices() {
-            // Swift 3まで
-            // if (d as AnyObject).position == AVCaptureDevicePosition.back {
-            if (d as AnyObject).position == AVCaptureDevice.Position.back {
-                // Swift 3まで
-                // device = d as? AVCaptureDevice
-                device = d as AVCaptureDevice
-                print("\(device!.localizedName) found.")
-            }
-        }
-        // バックカメラからキャプチャ入力生成
-        // Swift 3まで
-        // let input: AVCaptureDeviceInput?
+        output = AVCaptureVideoDataOutput()
+        device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
         guard let input = try? AVCaptureDeviceInput(device: device) else {
             print("Caught exception!")
             return
         }
         session.addInput(input)
-        output = AVCaptureStillImageOutput()
         session.addOutput(output)
-        // Swift 3まで
-        // session.sessionPreset = AVCaptureSessionPresetPhoto
+
         session.sessionPreset = .photo
-        // プレビューレイヤを生成
         let previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        // Swift 3まで
-        // previewLayer?.frame = view.bounds
-        // view.layer.addSublayer(previewLayer!)
         previewLayer.frame = view.bounds
         view.layer.addSublayer(previewLayer)
+
         // セッションを開始
         session.startRunning()
+
         // 撮影ボタンを生成
         let button = UIButton()
         button.setTitle("撮影", for: .normal)
@@ -73,18 +56,8 @@ class ViewController: UIViewController {
     // Swift 3まで
     // func shot(_ sender: AnyObject) {
     @objc func shot(_ sender: AnyObject) {
-        // Swift 3まで
-        // let connection = output.connection(withMediaType: AVMediaTypeVideo)
-        // output.captureStillImageAsynchronously(from: connection) {(imageDataBuffer, error) -> Void in
-        //     let imageData: Data = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataBuffer)
-        let connection = output.connection(with: .video)
-        output.captureStillImageAsynchronously(from: connection!) {(imageDataBuffer, error) -> Void in
-            if error == nil, let imageData: Data = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataBuffer!) {
-                let image = UIImage(data: imageData)!
-                // 画像をカメラロールに保存
-                UIImageWriteToSavedPhotosAlbum(image, self, nil, nil)
-            }
-        }
+        //let connection = output.connection(with: .video)
+
     }
 }
 
