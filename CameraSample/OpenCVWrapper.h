3aeff831019e49e735b30597d06c41a1e02d5238
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2018-09-13 19:38:04 +0900

    Add bridging classes

---
+++ CameraSample/OpenCVWrapper.h
@@ -0,0 +1,13 @@
+//
+//  OpenCVWrapper.h
+//  CameraSample
+//
+//  Created by Naoyoshi Aikawa on 2018/09/12.
+//  Copyright © 2018年 Naoyoshi Aikawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenCVWrapper : NSObject
+
+@end
---
+++ CameraSample/OpenCVWrapper.mm
@@ -0,0 +1,13 @@
+//
+//  OpenCVWrapper.m
+//  CameraSample
+//
+//  Created by Naoyoshi Aikawa on 2018/09/12.
+//  Copyright © 2018年 Naoyoshi Aikawa. All rights reserved.
+//
+
+#import "OpenCVWrapper.h"
+
+@implementation OpenCVWrapper
+
+@end
